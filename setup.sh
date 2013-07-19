7b2eb9be8825000eaf0ec9764894eeef445275d4
Dylan Turner <dylan.turner22@gmail.com>
2013-07-19 11:40:55 -0600

    changed to my dotfile repo

--- setup.sh
+++ setup.sh
@@ -34,7 +34,7 @@ fi
 if [ -d .emacs.d/ ]; then
     mv .emacs.d .emacs.d~
 fi
-git clone https://github.com/physiocoder/dotfiles.git
+git clone https://github.com/dylntnr/dotfiles.git
 ln -sb dotfiles/.screenrc .
 ln -sb dotfiles/.bash_profile .
 ln -sb dotfiles/.bashrc .
